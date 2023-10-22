d87fe328ccc2edbe5e6e778f7efa256a22bc0941
Piotr Baraniak <piterb86@gmail.com>
2023-10-22 10:50:24 +0200

    first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import { Container } from './Container';
+
+import { Profile } from './Profile';
+import user from '../configs/user';
+
+import { Statistics } from './Statistics';
+import data from '../configs/data';
+
+import { FriendList } from './FriendList';
+import friends from '../configs/friends';
+
+import { TransactionHistory } from './TransactionHistory';
+import transactions from '../configs/transactions';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </Container>
   );
 };
